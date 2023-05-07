db39a63653c83f7611ea4b6a67955c532611d958
Sourcery AI <>
2023-05-07 19:50:08 +0000

    'Refactored by Sourcery'

--- predict.py
+++ predict.py
@@ -156,4 +156,4 @@ def clean_folder(folder):
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
         except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+            print(f'Failed to delete {file_path}. Reason: {e}')
